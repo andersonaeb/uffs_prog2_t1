1c84c50704378406d6859a87df419266178b3f43
anderson <anderson@anderson>
2012-12-12 17:42:22 -0200

    Correção de bug.

--- library/Board.php
+++ library/Board.php
@@ -15,13 +15,10 @@ class Board extends Model {
     //put your code here    
     protected $matrix;
     protected $save = false;
-    
-    
 
     public function __construct($params = array()) {
         parent::__construct($params);
         $this->matrix = array();
-                
     }
 
     public function savePosition($line, $column, $value) {
@@ -32,6 +29,23 @@ class Board extends Model {
         return (isset($this->matrix[$line][$column]) ? $this->matrix[$line][$column] : null);
     }
 
+    public function verifyCompleteMatrix() {
+        
+        $c = 0;
+        for ($i = 0; $i < sizeof($this->matrix); $i++) {            
+            if (isset($this->matrix[$i])) {
+                for ($j = 0; $j < sizeof($this->matrix[$i]); $j++) {
+                    $c++;
+                }                
+            }
+        }
+        
+        if($c == 9)
+            return true;
+        else
+            return false;
+    }
+
     public function verifyIsWin() {
 
         $win = false;
@@ -39,24 +53,22 @@ class Board extends Model {
         for ($i = 0; $i < sizeof($this->matrix); $i++) {
             $temp = null;
             $count = 1;
-            
-            if(isset($this->matrix[$i])) {
+
+            if (isset($this->matrix[$i])) {
                 for ($j = 0; $j < sizeof($this->matrix[$i]); $j++) {
 
                     if (isset($this->matrix[$i][$j]) && $this->matrix[$i][$j] != $temp) {
                         $temp = $this->matrix[$i][$j];
-
-                    }
-                    else
-                        if(isset($this->matrix[$i][$j]))
-                            $count++;
-                }            
+                    } else
+                    if (isset($this->matrix[$i][$j]))
+                        $count++;
+                }
                 if ($count >= 3) {
                     $win = $this->matrix[$i][0];
                 }
             }
         }
-        
+
         for ($i = 0; $i < 3; $i++) {
             $temp = null;
             $count = 1;
@@ -65,29 +77,35 @@ class Board extends Model {
 
                 if (isset($this->matrix[$j][$i]) && $this->matrix[$j][$i] != $temp) {
                     $temp = $this->matrix[$j][$i];
-                    
-                }
-                else
-                    if(isset($this->matrix[$j][$i]))
-                        $count++;
-            }            
-            if ($count >= 3) {                
+                } else
+                if (isset($this->matrix[$j][$i]))
+                    $count++;
+            }
+            if ($count >= 3) {
                 $win = $this->matrix[0][$i];
             }
         }
-        
-        
-        if(isset($this->matrix[0][0]) && isset($this->matrix[1][1]) && isset($this->matrix[2][2]) && $this->matrix[0][0] == $this->matrix[1][1] && $this->matrix[1][1] == $this->matrix[2][2]) {
+
+
+        if (isset($this->matrix[0][0]) && isset($this->matrix[1][1]) && isset($this->matrix[2][2]) && $this->matrix[0][0] == $this->matrix[1][1] && $this->matrix[1][1] == $this->matrix[2][2]) {
             $win = $this->matrix[0][0];
         }
-      
-        if(isset($this->matrix[0][2]) && isset($this->matrix[1][1]) && isset($this->matrix[2][0]) && $this->matrix[0][2] == $this->matrix[2][0] && $this->matrix[2][0] == $this->matrix[1][1]) {
+
+        if (isset($this->matrix[0][2]) && isset($this->matrix[1][1]) && isset($this->matrix[2][0]) && $this->matrix[0][2] == $this->matrix[2][0] && $this->matrix[2][0] == $this->matrix[1][1]) {
             $win = $this->matrix[0][2];
         }
-        
+
+        /*
+         * Verify is tied
+         */
+
+        if ($this->verifyCompleteMatrix() && !$win) {
+            $win = 3;
+        }
+
         return $win;
     }
-    
+
     public function getSave() {
         return $this->save;
     }
@@ -96,6 +114,4 @@ class Board extends Model {
         $this->save = $save;
     }
 
-
-    
 }
